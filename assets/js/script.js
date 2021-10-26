a98a52178cafdc86265bafd6a8f26ce8c60738e6
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-10-26 15:56:23 -0400

    check that user has set minimum parameter

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,8 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
 
+function generatePassword() {
   var passLength = prompt("How long would you like your password to be? (8-128 characters)")
   if (passLength < 8 || passLength > 128) {
     alert("Please provide a value between 8-128 characters for your password length.");
@@ -28,71 +28,88 @@ function writePassword() {
   } else if (isNaN(passLength)) {
     alert("Please provide a value between 8-128 characters for your password length.");
     writePassword();
-    } else {
-    passLength = String(passLength);
-  }
+    } 
+  
+
 
   var lowerConfirm = prompt("Would you like lowercase letters in your password? Please type 'yes' or 'no'.");
   lowerConfirm = lowerConfirm.toLowerCase();
   if (lowerConfirm === 'yes') {
     var lowerConfirm = 'abcdefghijklmnopqrstuvwxyz';
   } else if (lowerConfirm === 'no') {
-    var lowerConfirm = "";
+    var lowerConfirm = '';
   } else {
     alert("Please provide a valid response.")
     writePassword();
     }
+
+
 
   var upperConfirm = prompt("Would you like uppercase letters in your password? Please type 'yes' or 'no'.");
   upperConfirm = upperConfirm.toLowerCase();
   if (upperConfirm === 'yes') {
     var upperConfirm = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   } else if (upperConfirm === 'no') {
-    var upperConfirm = "";
+    var upperConfirm = '';
   } else {
     alert("Please provide a valid response.")
     writePassword();
   }
+
+
 
   var numConfirm = prompt("Would you like numbers in your password? Please type 'yes' or 'no'.");
   numConfirm = numConfirm.toLowerCase();
   if (numConfirm === 'yes') {
     var numConfirm = '1234567890';
   } else if (numConfirm === 'no') {
-    var numConfirm = "";
+    var numConfirm = '';
   } else {
     alert("Please provide a valid response.")
     writePassword();
     }
+
+
 
   var charConfirm = prompt("Would you like special characters in your password? Please type 'yes' or 'no'.");
   charConfirm = charConfirm.toLowerCase();
   if (charConfirm === 'yes') {
     var charConfirm = '!"#$%&()*+,-/:;<=>?@[`]^_{}~';
   } else if (charConfirm === 'no') {
-    var charConfirm = "";
+    var charConfirm = '';
   } else {
     alert("Please provide a valid response.")
     writePassword();
     }
-
+  
+  // check the user has entered at least one parameter
+  if (lowerConfirm.length > 2 || upperConfirm.length > 2 
+    || numConfirm.length > 2 || charConfirm.length > 2)  {
     
-  function generatePassword() {
-      var password = String(lowerConfirm + upperConfirm + numConfirm + charConfirm);
-      var result = '';
-      for ( var i = 0; i < passLength; i++ ) {
-        result += password.charAt(Math.random() * (passLength * 10));
-      }
-      return result;
+    var password = String(lowerConfirm + upperConfirm + numConfirm + charConfirm);
+    console.log(password);
+    var result = '';
+    for ( var i = 0; i < passLength; i++ ) {
+      result += password.charAt(Math.random() * (passLength * 10));
+    }
+    return result;
+  } else {
+    alert("You must choose at least one parameter to include in your password.")
+    writePassword();
+    return;
   }
-    // why when passLength = 10, password is 9 characters? 
     
+    // why when passLength = 10, password is 9 characters? 
+  
+}
+function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   
   console.log(password);
-
+  return;
 }
 
 // Add event listener to generate button
